bbdc6df34994063377c6e74c168c200cf35dce0b
Veerabahu Subramanian <vsubramanian@linkedin.com>
2018-02-10 07:17:35 +0530

    Cleanup

--- client/components/AppHeader.jsx
+++ client/components/AppHeader.jsx
@@ -2,42 +2,28 @@
 
 import React from "react";
 import { withStyles } from "material-ui/styles";
-// import Appbar from "muicss/lib/react/appbar";
 import Toolbar from "material-ui/Toolbar";
 import Typography from "material-ui/Typography";
 import AppBar from "material-ui/AppBar";
 import Button from "material-ui/Button";
-// import { Link } from "react-router-dom";
 
-// const AppHeader = () => (
-//   <header className="header">
-//     <Appbar className="mui--appbar-line-height">
-//       <div className="mui-container-fluid">
-//         <span className="header-nav-items">
-//           <Link to="/roster">Roster</Link>
-//         </span>
-//         <span className="header-nav-items">
-//           <Link to="/leavesummary">Leave Summary</Link>
-//         </span>
-//         <span className="mui--pull-right header-nav-items">Login</span>
-//       </div>
-//     </AppBar>
-//   </header>
-// );
 const styles = {
   root: {
     width: "100%"
   },
   flex: {
     flex: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
   }
 };
 
-const AppHeader = props => {
+type Props = {
+  classes: {
+    root: {},
+    flex: {}
+  }
+};
+
+const AppHeader = (props: Props) => {
   const { classes } = props;
   return (
     <div className={classes.root}>
@@ -52,8 +38,13 @@ const AppHeader = props => {
             <Button color="inherit" href="/roster">
               Roster
             </Button>
+            <Button color="inherit" href="/leavesummary">
+              Leave Summary
+            </Button>
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Typography variant="title" color="inherit" gutterBottom>
+            <Button color="inherit">Login</Button>
+          </Typography>
         </Toolbar>
       </AppBar>
     </div>
